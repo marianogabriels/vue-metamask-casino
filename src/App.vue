689a45b5ae951971fd157060a70b2ae39d6eb325
Mariano <marianogabriels@gmail.com>
2018-10-21 14:16:35 -0300

    Add web3

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,47 @@
 <template>
     <div id="app">
-        hello
+        <div className="block">
+            <h4>Timer:</h4> &nbsp;
+            <span ref="timer"> {{this.timer}}</span>
+        </div>
+        <div className="block">
+            <h4>Last winner:</h4> &nbsp;
+            <span ref="last-winner">{{this.lastWinner}}</span>
+        </div>
+
+        <h2>Vote for the next number</h2>
+        <ul>
+            <li @click="vote(1)"> 1</li>
+            <li @click="vote(2)"> 2</li>
+            <li @click="vote(3)"> 3</li>
+            <li @click="vote(4)"> 4</li>
+            <li @click="vote(5)"> 5</li>
+            <li @click="vote(6)"> 6</li>
+            <li @click="vote(7)"> 7</li>
+            <li @click="vote(8)"> 8</li>
+            <li @click="vote(9)"> 9</li>
+            <li @click="vote(10)"> 10</li>
+        </ul>
     </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Web3 from 'web3'
 
 export default {
     name: 'app',
+    data: function(){
+        return({
+            timer: 0,
+            lastWinner: 0
+        })
+    },
+    methods: {
+        vote: function(value) {
+            console.log(value)
+        }
+    },
     components: {
         HelloWorld
     }
@@ -23,5 +56,24 @@ export default {
     text-align: center;
     color: #2c3e50;
     margin-top: 60px;
+}
+ul{
+    list-style-type: none;
+    padding-left: 0;
+    display: flex;
+}
+li{
+    padding: 40px;
+    border: 2px solid rgb(30,134,255);
+    margin-right: 5px;
+    border-radius: 10px;
+    cursor: pointer;
+}
+li:hover{
+    background-color: rgb(30,134,255);
+    color: white;
+}
+li:active{
+    opacity: 0.7;
 }
 </style>
