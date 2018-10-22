be9b0e5bdb3588724b5419e133a865ed32799cde
Mariano <marianogabriels@gmail.com>
2018-10-22 01:14:52 -0300

    Support for bet

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <template>
     <div id="app">
         <div className="block">
+            <h4>Current Address</h4> &nbsp;
+            <span v-if='this.address' ref="last-winner">{{this.address}}</span>
+            <span v-else='this.address' ref="last-winner">loading...</span>
+        </div>
+
+        <div className="block">
+            <h4>Number Of Bets</h4> &nbsp;
+            <span v-if='this.address' ref="last-winner">{{this.numberOfBets}}</span>
+            <span v-else='this.address' ref="last-winner">loading...</span>
+        </div>
+
+        <div className="block">
             <h4>Timer:</h4> &nbsp;
-            <span ref="timer"> {{this.timer}}</span>
+            <span ref="timer"> ...</span>
         </div>
         <div className="block">
             <h4>Last winner:</h4> &nbsp;
@@ -30,17 +42,16 @@ import HelloWorld from './components/HelloWorld.vue'
 import Web3 from 'web3'
 import CasinoAbi from './CasinoAbi'
 
-if(typeof web3 != 'undefined'){
+if(typeof window.web3 != 'undefined'){
     console.log("Using web3 detected from external source like Metamask")
-    var web3 = new Web3(web3.currentProvider)
+    var web3 = new Web3(window.web3.currentProvider)
 }else{
     var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
 }
 
 
-//const MyContract = new web3.eth.Contract(CasinoAbi)
-const Casino = new web3.eth.Contract(CasinoAbi, '0xa106e4abca8fd07272715976ec818a5d65ba06c3')
 
+const Casino = new web3.eth.Contract(CasinoAbi, '0xa4f60d72327ef9edc45d3bca51580f87af388d36')
 
 export default {
     name: 'app',
@@ -48,6 +59,7 @@ export default {
         return({
             lastWinner: 0,
             numberOfBets: 0,
+            address: null,
             minimumBet: 0,
             totalBet: 0,
             maxAmountOfBets: 0,
@@ -55,8 +67,18 @@ export default {
     },
     methods: {
         vote: function(value) {
-            console.log(value)
+            Casino.methods.bet(value).send({
+                gas: 100000,
+                from: this.address,
+                value: web3.utils.toWei(String(0.1), 'ether')
+            })
         }
+    },
+    mounted: function(){
+        web3.eth.getAccounts().then(function(e) { this.address = e[0] }.bind(this) )
+        Casino.methods.numberOfBets().call().then((e) => {
+            this.numberOfBets = e
+        })
     },
     components: {
         HelloWorld
